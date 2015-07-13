91d42fb18f823b8b4c51dcf4e6da3fcba2be4826
Damyan Pepper <damyanp@microsoft.com>
2015-07-13 10:49:20 -0700

    Cleaner way to disable min/max macros

--- SvgWin2D/SvgWin2D.Shared/pch.h
+++ SvgWin2D/SvgWin2D.Shared/pch.h
@@ -1,12 +1,11 @@
 ﻿#pragma once
 
 #define _CRT_SECURE_NO_WARNINGS 1
+#define NOMINMAX 1
 
 #include <collection.h>
 #include <ppltasks.h>
 
 #undef small
-#undef min
-#undef max
 
 #include <svgpp/svgpp.hpp>
