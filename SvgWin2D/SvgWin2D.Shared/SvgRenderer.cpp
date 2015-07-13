f71eb45ef2bdd1f4d0a803764d943c543c7fd309
Damyan Pepper <damyanp@microsoft.com>
2015-07-13 11:03:45 -0700

    Stub SvgRenderer.Render

--- SvgWin2D/SvgWin2D.Shared/SvgRenderer.cpp
+++ SvgWin2D/SvgWin2D.Shared/SvgRenderer.cpp
@@ -2,9 +2,18 @@
 
 namespace SvgWin2D
 {
-    public ref class SvgRenderer
+    using namespace Microsoft::Graphics::Canvas;
+    using namespace Windows::Data::Xml::Dom;
+
+    public ref class SvgRenderer sealed
     {
     public:
-        
+        CanvasCommandList^ Render(ICanvasResourceCreator^ resourceCreator, XmlDocument^ svgDocument)
+        {
+            auto commandList = ref new CanvasCommandList(resourceCreator);
+            auto ds = commandList->CreateDrawingSession();
+            ds->DrawText(L"Hello, world", 0, 0, Windows::UI::Colors::White);
+            return commandList;
+        }
     };
 }
